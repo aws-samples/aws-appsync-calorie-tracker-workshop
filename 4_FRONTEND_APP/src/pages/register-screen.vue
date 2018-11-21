3b6c534f50707d6eaac229c5b1849dcd47b2c375
Jupiter <jupitera@8c8590268ae9.ant.amazon.com>
2018-11-21 23:07:28 +0000

    Toaster bug fix + instructions are clearer

--- 4_FRONTEND_APP/src/pages/register-screen.vue
+++ 4_FRONTEND_APP/src/pages/register-screen.vue
@@ -5,20 +5,20 @@
     <f7-list no-hairlines-md>
     <f7-list-item>
         <f7-icon icon="demo-list-icon" slot="media"></f7-icon>
-        <f7-label>Name</f7-label>
-        <f7-input :value="username" @input="username = $event.target.value" type="text" placeholder="Your name" info='First name should be enough' required validate clear-button></f7-input>
+        <f7-label>Username</f7-label>
+        <f7-input :value="username" @input="username = $event.target.value" type="text" placeholder="" info="Watch out! It's case sensetive" required validate clear-button></f7-input>
     </f7-list-item>
 
     <f7-list-item>
         <f7-icon icon="demo-list-icon" slot="media"></f7-icon>
         <f7-label>E-mail</f7-label>
-        <f7-input :value="email" @input="email = $event.target.value" type="email" placeholder="Your e-mail" info="We'll need your real email for verification" required validate clear-button></f7-input>
+        <f7-input :value="email" @input="email = $event.target.value" type="email" placeholder="" info="We'll need your real email for verification" required validate clear-button></f7-input>
     </f7-list-item>
 
     <f7-list-item>
         <f7-icon icon="demo-list-icon" slot="media"></f7-icon>
         <f7-label>Password</f7-label>
-        <f7-input :value="password" @input="password = $event.target.value" type="password" placeholder="Should be [details here]" info="wut?" required validate clear-button></f7-input>
+        <f7-input :value="password" @input="password = $event.target.value" type="password" placeholder="" info="Password must conform with Cognito's policy" required validate clear-button></f7-input>
     </f7-list-item>
     </f7-list>
     <div class="block block-strong row">
@@ -69,14 +69,13 @@ export default {
           self.$f7.preloader.hide()
 
           // Create toast with error message
-          if (!self.errorToasterMessage) {
-            self.errorToasterMessage = self.$f7.toast.create({
-              closeButton: true,
-              text: err.code + ':' + err.message,
-              closeTimeout: 12000,
-              destroyOnClose: true
-            })
-          }
+          self.errorToasterMessage = self.$f7.toast.create({
+            closeButton: true,
+            text: err.code + ':' + err.message,
+            closeTimeout: 12000,
+            destroyOnClose: true
+          })
+
           // Open toast
           self.errorToasterMessage.open();
         })
